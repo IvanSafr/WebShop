d06519682bfc2a17288f32f5a3596fd4f789cd29
ivans <ivansafronov997@gmail.com>
2023-03-28 01:42:01 +0300

    0.2 version (db 2.0, forms 1.0)

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-db = SQLAlchemy
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
 
 class  product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    description = db.Column(db.Text)
-    price = db.Column(db.Integer)
-    availability = 0
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    availability = db.Column(db.Boolean, default=True)
 
 @app.route('/')
 def main():
@@ -20,7 +22,9 @@ def main():
 def about():
     return render_template('about.html')
 
-
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 
 if __name__=='__main__':
