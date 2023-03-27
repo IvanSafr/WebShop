f8ad05106701f583f7cb46c48f37d7b835cc5ac1
ivans <ivansafronov997@gmail.com>
2023-03-27 22:58:14 +0300

    0.2 version (early db)

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy
+
+class  product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.Text)
+    price = db.Column(db.Integer)
+    availability = 0
 
 @app.route('/')
 def main():
