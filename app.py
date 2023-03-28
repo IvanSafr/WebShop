8effb8e56c9f433d29d54c7a52cf2e34dffb85cc
ivans <ivansafronov997@gmail.com>
2023-03-28 12:08:14 +0300

    0.2.2 version (db 2.0, forms 1.0)

--- app.py
+++ app.py
@@ -14,17 +14,40 @@ class  product(db.Model):
     price = db.Column(db.Integer, nullable=False)
     availability = db.Column(db.Boolean, default=True)
 
+    def __repr__(self):
+        return f"Название: {self.name}"
+
 @app.route('/')
 def main():
     return render_template('main.html')
+
+@app.route('/catalog')
+def catalog():
+    products = product.query.all()
+    return render_template('catalog.html', data = products)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/add')
+@app.route('/add', methods=['POST', 'GET'])
 def add():
-    return render_template('add.html')
+    if request.method=="POST":
+        name = request.form['name']
+        description = request.form['description']
+        price = request.form['price']
+
+        Product = product(name=name, description=description, price=price)
+
+        try:
+            db.session.add(Product)
+            db.session.commit()
+            return redirect('/catalog')
+        except:
+            return "Еклмн, ошибка"
+
+    else:
+        return render_template('add.html')
 
 
 if __name__=='__main__':
